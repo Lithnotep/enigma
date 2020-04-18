bd3724ec42697c53accefc084ab4b348ba5ce809
Max Mitrani <mjmitrani168@gmail.com>
2020-04-18 16:20:20 -0600

    Add attribute test for date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,13 @@
 require 'pry'
+require 'date'
 
 class Enigma
+  attr_reader :date
 
+  def initialize
+    @date = DateTime.now.strftime("%d%m%Y")
+  end
 
 end
+
+# alphabet set ("a".."z").to_a << " "
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,9 +5,15 @@ require 'pry'
 
 class EnigmaTest < Minitest::Test
 
-def test_it_exists
-  enigma = Enigma.new
-  assert_instance_of Enigma, enigma
-end
+  def setup
+    @enigma = Enigma.new
+  end
 
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_has_attributes
+    assert_equal "18042020", @enigma.date
+  end
 end
