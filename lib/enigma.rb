a25fadc6e51fdadbf0514759b6ce826de586031a
Max Mitrani <mjmitrani168@gmail.com>
2020-04-20 15:46:55 -0600

    Add readability changes for enigma and removed comments

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,17 +5,13 @@ require 'pry'
 require 'date'
 
 class Enigma
-  attr_reader :offsets, :char_set, :ashift, :bshift, :cshift, :dshift, :shift, :key, :date
+  attr_reader :offsets, :char_set, :shift, :key, :date
 
   def initialize
     @char_set = ("a".."z").to_a << " "
     @date = DateTime.now.strftime("%d%m%y")
     @key = Key.new
     @shift = Shift.new
-    # @shift.ashift = shift_create
-    # @shift.bshift = shift_create
-    # @shift.bshift = shift_create
-    # @shift.bshift = shift_create
   end
 
   def encrypt(message, key, date = @date)
@@ -38,53 +34,10 @@ class Enigma
     offset
   end
 
-  # def shift_create
-  #   char_hash = Hash.new
-  #   @char_set.each do |char|
-  #     char_hash[char] = nil
-  #   end
-  #   char_hash
-  # end
-
   def offset_combine(offset, key)
     combine = [offset, key]
     combine.transpose.map(&:sum)
   end
-
-  # def full_shift_assign(code_shift)
-  #   ashift_assign(code_shift)
-  #   bshift_assign(code_shift)
-  #   cshift_assign(code_shift)
-  #   dshift_assign(code_shift)
-  # end
-  #
-  # def ashift_assign(code_shift)
-  #   paired_chars_a = @char_set.zip(@char_set.rotate(code_shift[0]).cycle)
-  #   paired_chars_a.each do |char|
-  #     @shift.ashift[char[0]] = char[1]
-  #   end
-  # end
-  #
-  # def bshift_assign(code_shift)
-  #   paired_chars_b = @char_set.zip(@char_set.rotate(code_shift[1]).cycle)
-  #   paired_chars_b.each do |char|
-  #     @shift.bshift[char[0]] = char[1]
-  #   end
-  # end
-  #
-  # def cshift_assign(code_shift)
-  #   paired_chars_c = @char_set.zip(@char_set.rotate(code_shift[2]).cycle)
-  #   paired_chars_c.each do |char|
-  #     @shift.bshift[char[0]] = char[1]
-  #   end
-  # end
-  #
-  # def dshift_assign(code_shift)
-  #   paired_chars_d = @char_set.zip(@char_set.rotate(code_shift[3]).cycle)
-  #   paired_chars_d.each do |char|
-  #     @shift.bshift[char[0]] = char[1]
-  #   end
-  # end
 
   def message_prep(message)
     message_feeder = []
