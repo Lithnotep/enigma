4a7fc3c8144fab96e6cb7da70e9b452d4b61e7bb
Max Mitrani <mjmitrani168@gmail.com>
2020-04-20 20:30:02 -0600

    Add incoming_message and encryption

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,11 +2,12 @@ require './lib/enigma'
 
 enigma = Enigma.new
 
-
-handle = File.open(AGRV[0], "r")
-
+handle = File.open(ARGV[0], "r")
 incoming_message = handle.read
-
 handle.close
+encryption = enigma.encrypt(incoming_message)
 
-writer = 
+writer = File.open(ARGV[1], "w")
+writer.write(encryption)
+
+p "Created 'encrypted.txt' with the key #{encryption[:key]} and date #{encryption[:date]}"
