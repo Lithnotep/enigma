72bf5adeea083eb6d2c28c5b5e127a16e2f6490e
Max Mitrani <mjmitrani168@gmail.com>
2020-04-20 20:46:54 -0600

    add read write and screen print functionality for decrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -8,6 +8,6 @@ handle.close
 encryption = enigma.encrypt(incoming_message)
 
 writer = File.open(ARGV[1], "w")
-writer.write(encryption)
+writer.write(encryption[:encryption])
 
 p "Created 'encrypted.txt' with the key #{encryption[:key]} and date #{encryption[:date]}"
